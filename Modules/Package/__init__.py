7fa14d658658ddcc82b737e72c8bff28d24fc2fc
sauvik007 <sahoo.souvik9791@gmail.com>
2024-12-20 20:52:59 +0530

    first commit

---
+++ Modules/Package/__init__.py
@@ -0,0 +1,1 @@
+"it a special file used in python to define packages and inital namespace"
---
+++ Modules/test.py
@@ -0,0 +1,6 @@
+from Package.maths import *
+from Package.subpackages.mult import multiply
+
+print(addition(2,3))
+print(substraction(4,3))
+print(multiply(4,5))
